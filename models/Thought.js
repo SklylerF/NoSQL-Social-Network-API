df5307cf1f792b84c3e73a5dcac069997a2e02f6
Skyler Ferreira <sky_3489@icloud.com>
2022-09-22 17:06:54 -0700

    created thoughts model

---
+++ models/Thought.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+
+// shema to create thought model
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => timeSince(date)
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [
+            {
+                type: Schema.types.objectId,
+                ref: 'reaction'
+            }]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false,
+    }
+);
+// retrives the length of reactions
+thoughtSchema
+    .virtuals('reactionsCount')
+    .get(() => this.reactions.length);
+
+//  initialize thought model
+const Thoughts = model('Thoughts', thoughtSchema);
+
+model.export = Thoughts;
